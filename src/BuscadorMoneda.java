b7c7e758f5ed14cb369d6431f4983e4ede6bf05f
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-04-23 13:09:29 -0400

    se crea clase Moneda y BuscadorMoneda

---
+++ src/BuscadorMoneda.java
@@ -0,0 +1,25 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.google.gson.Gson;
+
+public class BuscadorMoneda {
+
+    public Moneda ingresoUsuario (int moneda){
+    URI direccion = URI.create("https://v6.exchangerate-api.com/v6/b968a5e4dca58bf40a81d984/latest/" + moneda);
+
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+                            .uri(direccion)
+                            .build();
+    HttpResponse<String> response;
+    try {
+        response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    } catch (Exception e) {
+        throw new RuntimeException("Moneda no encontrada");
+    }
+    return new Gson().fromJson(response.body(), Moneda.class);
+    }
+}
