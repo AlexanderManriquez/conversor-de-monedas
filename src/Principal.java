4b4bf3d7d8581d5b35de08f0a9920829924c3dda
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-04-24 14:03:13 -0400

    se actualiza principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -3,14 +3,23 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         BuscadorMoneda buscadorMoneda = new BuscadorMoneda();
+        double cantidadOrigen;
+        String monedaOrigen;
+        String monedaDestino;
 
+        Scanner entrada = new Scanner(System.in);
         
-        // Convertir 100 dólares a euros
-        double cantidadDolares = 100;
-        String monedaOrigen = "USD";
-        String monedaDestino = "EUR";
-        double cantidadEuros = buscadorMoneda.convertirMoneda(cantidadDolares, monedaOrigen, monedaDestino);
-        System.out.println(cantidadDolares + " " + monedaOrigen + " son " + cantidadEuros + " " + monedaDestino);
+        System.out.println("Ingresa la moneda de Origen: ");
+        monedaOrigen = entrada.next();
+        System.out.println("Ingresa la moneda de destino: ");
+        monedaDestino = entrada.next();
+        System.out.println("Que cantidad deseas convertir: ");
+        cantidadOrigen = entrada.nextDouble();
+        
+        double cantidadDestino = buscadorMoneda.convertirMoneda(cantidadOrigen, monedaOrigen, monedaDestino);
+        System.out.println(cantidadOrigen + " " + monedaOrigen + " son " + cantidadDestino + " " + monedaDestino);
+
+        entrada.close();
     }
     
 }
