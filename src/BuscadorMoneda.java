2e45817ec6d92149ebd3794d3d1cd0d5cb0214db
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-04-24 14:10:23 -0400

    se modifica BuscadorMoneda.java

--- src/BuscadorMoneda.java
+++ src/BuscadorMoneda.java
@@ -22,7 +22,7 @@ public class BuscadorMoneda {
     // Método para obtener los datos de la moneda desde la API
     public Moneda ingresoUsuario(String moneda) {
         URI direccion = URI.create("https://v6.exchangerate-api.com/v6/b968a5e4dca58bf40a81d984/latest/" + moneda);
-
+    
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(direccion)
@@ -30,9 +30,20 @@ public class BuscadorMoneda {
         HttpResponse<String> response;
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String responseBody = response.body();
+            Gson gson = new Gson();
+            Moneda monedaData = gson.fromJson(responseBody, Moneda.class);
+            
+            // Verificar si el mapa de tasas de conversión se ha inicializado correctamente
+            if (monedaData.getConversionRates() == null) {
+                throw new RuntimeException("El objeto Moneda devuelto no tiene tasas de conversión.");
+            }
+            
+            return monedaData;
         } catch (Exception e) {
             throw new RuntimeException("Moneda no encontrada");
         }
-        return new Gson().fromJson(response.body(), Moneda.class);
     }
+    
 }
+
