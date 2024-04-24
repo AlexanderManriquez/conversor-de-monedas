b7774194465858d96aa4f526c7283ba02fa2f2e7
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-04-24 14:23:12 -0400

    se modifica Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         BuscadorMoneda buscadorMoneda = new BuscadorMoneda();
-        double cantidadOrigen;
-        String monedaOrigen;
-        String monedaDestino;
-
         Scanner entrada = new Scanner(System.in);
         
-        System.out.println("Ingresa la moneda de Origen: ");
-        monedaOrigen = entrada.next();
-        System.out.println("Ingresa la moneda de destino: ");
-        monedaDestino = entrada.next();
-        System.out.println("Que cantidad deseas convertir: ");
-        cantidadOrigen = entrada.nextDouble();
+        System.out.println("Bienvenido al conversor de monedas");
+        System.out.println("Por favor, ingrese la moneda de origen:");
+        String monedaOrigen = entrada.nextLine();
         
-        double cantidadDestino = buscadorMoneda.convertirMoneda(cantidadOrigen, monedaOrigen, monedaDestino);
-        System.out.println(cantidadOrigen + " " + monedaOrigen + " son " + cantidadDestino + " " + monedaDestino);
-
+        System.out.println("Ingrese la moneda de destino:");
+        String monedaDestino = entrada.nextLine();
+        
+        System.out.println("Ingrese la cantidad de dinero a convertir:");
+        double cantidad = entrada.nextDouble();
+        
+        double cantidadConvertida = buscadorMoneda.convertirMoneda(cantidad, monedaOrigen, monedaDestino);
+        
+        System.out.println(cantidad + " " + monedaOrigen + " equivale a " + cantidadConvertida + " " + monedaDestino);
+        
         entrada.close();
     }
-    
 }
