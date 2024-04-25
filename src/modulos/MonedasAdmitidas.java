82d0295b48b708d02e7c9f7ca4268fa4619bf377
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-04-25 13:25:23 -0400

    se crea lista monedas admitidas

--- src/modulos/MonedasAdmitidas.java
+++ src/modulos/MonedasAdmitidas.java
@@ -1,5 +1,38 @@
 package modulos;
 
-public class MonedasAdmitidas {
+import java.util.HashSet;
+import java.util.Set;
 
+public class MonedasAdmitidas {
+    private static Set<String> monedasAdmitidas;
+
+    static {
+        // Inicializar el conjunto de monedas admitidas
+        monedasAdmitidas = new HashSet<>();
+        monedasAdmitidas.add("USD");
+        monedasAdmitidas.add("EUR");
+        monedasAdmitidas.add("GBP");
+        monedasAdmitidas.add("CLP");
+        monedasAdmitidas.add("ARS");
+        monedasAdmitidas.add("BOB");
+        monedasAdmitidas.add("PEN");
+        monedasAdmitidas.add("BRL");
+        monedasAdmitidas.add("COP");
+        monedasAdmitidas.add("MXN");
+        monedasAdmitidas.add("CNY");
+        monedasAdmitidas.add("JPY");
+    }
+
+    public static Set<String> getMonedasAdmitidas() {
+        return monedasAdmitidas;
+    }
+
+    public static void mostrarMonedasAdmitidas() {
+        System.out.println("Monedas admitidas para conversión:");
+        for (String moneda : monedasAdmitidas) {
+            System.out.println(moneda);
+        }
+    }
 }
+
+
