03acb9f4a6777cb0047c6f751e37254b6a8ab9b8
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-04-28 15:11:37 -0400

    se modifica MonedasAdmitidas.java

--- src/modulos/MonedasAdmitidas.java
+++ src/modulos/MonedasAdmitidas.java
@@ -27,11 +27,29 @@ public class MonedasAdmitidas {
         return monedasAdmitidas.contains(moneda);
     }
 
+    public static String getNombreMoneda(String codigoMoneda) {
+        switch (codigoMoneda) {
+            case "USD": return "USD = Dólar estadounidense";
+            case "EUR": return "EUR = Euro";
+            case "GBP": return "GBP = Libra esterlina";
+            case "CLP": return "CLP = Peso chileno";
+            case "ARS": return "ARS = Peso argentino";
+            case "BOB": return "BOB = Boliviano";
+            case "PEN": return "PEN = Sol peruano";
+            case "BRL": return "BRL = Real brasileño";
+            case "COP": return "COP = Peso colombiano";
+            case "MXN": return "MXN = Peso mexicano";
+            case "CNY": return "CNY = Yuan chino";
+            case "JPY": return "JPY = Yen japonés";
+            default: return "Desconocido";
+        }
+    }
+
     public static void mostrarMonedasAdmitidas() {
         System.out.println("Monedas admitidas para conversión:");
         int index = 1;
         for (String moneda : monedasAdmitidas) {
-        System.out.println(index + ". " + moneda);
+        System.out.println(index + ". " + getNombreMoneda(moneda));
         index++;
         }
     }
